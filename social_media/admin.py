f9a4a977a8895a47fbd6d0a40a4b7d8cfbded909
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-09-17 13:55:21 +0300

    Register models to admin panel

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import Profile, Post, Comment, Like, Message
+
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Message)
